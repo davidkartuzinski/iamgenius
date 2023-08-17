b7a46419141fdd368c4202047c47d285c45e9031
davidkartuzinski <david@kaidawei.me>
2023-08-18 00:46:35 +0200

    logo resized

--- iamgenius/app/components/Logo/index.js
+++ iamgenius/app/components/Logo/index.js
@@ -1,14 +1,14 @@
-import Image from "next/image";
-import logo from "./logo.png";
+import Image from 'next/image';
+import logo from './logo.png';
 
-const Logo = ({ width = "63.5", height = "75" }) => {
+const Logo = ({ width = '133', height = '55' }) => {
   return (
     <Image
       priority
       src={logo}
       width={width}
       height={height}
-      alt="I am Genius logo"
+      alt='I am Genius logo'
     />
   );
 };
