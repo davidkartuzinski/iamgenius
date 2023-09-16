f3401036fb6cc2ca54779bb3d57fd53c0c43bad0
davidkartuzinski <david@kaidawei.me>
2023-09-16 18:48:01 +0200

    start of migration to new translations package

--- iamgenius/app/components/server-side/Trait/index.js
+++ iamgenius/app/components/server-side/Trait/index.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 //https://levelup.gitconnected.com/accessible-anchor-links-a-key-element-of-inclusive-web-design-30e6d786fec9
 // https://amberwilson.co.uk/blog/are-your-anchor-links-accessible/
 
-function Trait({ number, name, content }) {
+function Trait({ number, name, children }) {
   return (
     <div id={`${slugify(name)}`} className={styles.traits}>
       <h3 id={`#${slugify(name)}-heading`}>
@@ -18,7 +18,7 @@ function Trait({ number, name, content }) {
           {number}. {name}.
         </Link>
       </h3>{' '}
-      <p>"{content}"</p>
+      {children}
     </div>
   );
 }
--- iamgenius/next.config.js
+++ iamgenius/next.config.js
@@ -1,12 +1,17 @@
 const nextConfig = {
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   experimental: {
     appDir: true,
+    mdxRs: true,
   },
+
   images: {
-    domains: ["iamgenius.io"],
+    domains: ['iamgenius.io'],
   },
+
   reactStrictMode: true,
   swcMinify: true,
 };
-
-module.exports = nextConfig;
+const withMDX = require('@next/mdx')();
+module.exports = withMDX(nextConfig);
