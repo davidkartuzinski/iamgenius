bf6ee64eb51ce722b7ad68a4197486cb36814684
davidkartuzinski <david@kaidawei.me>
2023-10-25 02:12:56 +0200

    Change hamburger icon to hamburger icon from Lucide Icons

--- iamgenius/app/components/server-side/IconLinks/index.js
+++ iamgenius/app/components/server-side/IconLinks/index.js
@@ -109,3 +109,32 @@ export const LinkIcon = ({
     </>
   );
 };
+
+export const HamburgerIcon = ({
+  width = '2rem',
+  height = '2rem',
+}) => {
+  return (
+    <span
+      style={{
+        display: 'block',
+        width: width,
+        height: height,
+      }}
+    >
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        className={styles.svg}
+        viewBox='0 0 20 20'
+        fill='#b01300'
+        aria-hidden='true'
+      >
+        <path
+          fillRule='evenodd'
+          d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z'
+          clipRule='evenodd'
+        />
+      </svg>
+    </span>
+  );
+};
